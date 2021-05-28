49d52e47961109c32c4b991adb9902fb0a47c17f
xiaofei <alibaba22001@163.com>
2021-05-28 14:54:04 +0800

    update

--- apps/uart_debug_handler.c
+++ apps/uart_debug_handler.c
@@ -6,11 +6,15 @@
  *   文件名称：uart_debug_handler.c
  *   创 建 者：肖飞
  *   创建日期：2020年05月13日 星期三 13时18分00秒
- *   修改日期：2020年12月29日 星期二 16时57分42秒
+ *   修改日期：2021年05月28日 星期五 14时48分53秒
  *   描    述：
  *
  *================================================================*/
 #include "uart_debug_handler.h"
+
+#include <time.h>
+#include <stdio.h>
+
 #include "test_event.h"
 #include "iap.h"
 
@@ -25,6 +29,44 @@ static void fn1(char *arguments)
 static void fn2(char *arguments)
 {
 	try_get_test_event();
+}
+
+//3 2021-05-28-14-16-02
+static void fn3(char *arguments)
+{
+	struct tm tm = {0};
+	char dt[20];
+	char bcd[8];
+
+	debug("arguments: '%s'", arguments);
+	sscanf(arguments, "%19s", dt);
+	_hexdump("dt", dt, sizeof(dt));
+	debug("dt: '%s'", dt);
+
+	debug("test strptime...");
+	strptime(dt, "%Y-%m-%d-%H-%M-%S", &tm);
+	debug("tm %04d-%02d-%02d %02d:%02d:%02d",
+	      tm.tm_year + 1900,
+	      tm.tm_mon + 1,
+	      tm.tm_mday,
+	      tm.tm_hour,
+	      tm.tm_min,
+	      tm.tm_sec);
+
+	debug("test strftime...");
+	strftime(dt, sizeof(dt), "%Y%m%d%H%M%S", &tm);
+	_hexdump("dt", dt, sizeof(dt));
+	debug("dt: '%s'", dt);
+
+	debug("test ascii_to_bcd...");
+	memset(bcd, 0xff, sizeof(bcd));
+	ascii_to_bcd(dt, strlen(dt), bcd, sizeof(bcd));
+	_hexdump("bcd", bcd, sizeof(bcd));
+
+	debug("test bcd_to_ascii...");
+	memset(dt, 0, sizeof(dt));
+	bcd_to_ascii(dt, sizeof(dt), bcd, sizeof(bcd));
+	_hexdump("dt", dt, sizeof(dt));
 }
 
 uint16_t osGetCPUUsage(void);
@@ -85,6 +127,7 @@ static void fn5(char *arguments)
 static uart_fn_item_t uart_fn_map[] = {
 	{1, fn1},
 	{2, fn2},
+	{3, fn3},
 	{5, fn5},
 };
 
